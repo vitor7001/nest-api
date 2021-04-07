e5658e304feb3aa98cb7858c204d18754b72ac39
vitorgomes7001 <vitorgomes7001@gmail.com>
2021-04-07 20:07:35 -0300

    feat: estruturando schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RoutesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    RoutesModule,
+    MongooseModule.forRoot(process.env.MONGO_DSN,{
+      useNewUrlParser: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
